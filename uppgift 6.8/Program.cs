a1d9aea304dc777f66c1756b51428901a3703d12
Turky0987 <144216105+Turky0987@users.noreply.github.com>
2024-02-28 10:23:45 +0100

    Add project files.

---
+++ uppgift 6.8/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ange ett heltal: ");
+        int användarensTal = Convert.ToInt32(Console.ReadLine());
+
+        int antalPrimtal = BeräknaAntalPrimtal(användarensTal);
+        Console.WriteLine("Antalet primtal mindre än " + användarensTal + " är: " + antalPrimtal);
+    }
+
+    static bool ÄrPrimtal(int tal)
+    {
+        if (tal <= 1)
+        {
+            return false;
+        }
+
+        for (int i = 2; i <= Math.Sqrt(tal); i++)
+        {
+            if (tal % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    static int BeräknaAntalPrimtal(int maxGräns)
+    {
+        int antalPrimtal = 0;
+
+        for (int i = 2; i < maxGräns; i++)
+        {
+            if (ÄrPrimtal(i))
+            {
+                antalPrimtal++;
+            }
+        }
+
+        return antalPrimtal;
+    }
+}
